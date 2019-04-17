b24a5ef6438208384c2b5afa5fbe1378d446b284
huangkui <huangkui@lepu.cn>
2019-04-17 10:03:50 +0800

    test

---
+++ base/runtime1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func sum(a []int, c chan int) {
+	total := 0
+	for _, v := range a {
+		total += v
+	}
+	c <- total  // send total to c
+	fmt.Println(total);
+}
+
+func main() {
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+
+	go sum(a[:len(a)/2], c) //7,2,8
+	go sum(a[len(a)/2:], c) //-9,4,0
+	x, y := <-c, <-c  // receive from c
+
+	fmt.Println(x, y)
+}
