43f1246b53284a9cb04b643b289c25cafa052b29
huangkui <huangkui@lepu.cn>
2019-04-22 14:14:03 +0800

    zc

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"./pack1"
+	"fmt"
+)
+
+func main()  {
+	fmt.Println(pack1.name)
+}
+
---
+++ pack1/pack1.go
@@ -0,0 +1,13 @@
+package pack1
+
+var name string="hk"
+
+var age int=18
+
+
+
+func intro() (string,int) {
+	return name,age
+}
+
+
