9860433e0c0c2a70c5619ff2b4989e4146af06d4
huangkui <huangkui@lepu.cn>
2019-04-22 09:46:45 +0800

    regexp

--- base/package/regexp.go
+++ base/package/regexp.go
@@ -1,4 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+)
+
+func main()  {
+	str:="abc12b2c123F1g1E12php,python,PhP"
+
+	p:=regexp.MustCompile(`(?i:[a-z]+?[\d+])`)
+
+	r:=p.FindAll([]byte(str),10)
+	for _,v:=range r{
+		fmt.Println(string(v))
+	}
+
+
+/*
+	pattern:="PHP"
+	//r 始终是nil,表示是否出现err
+	if ok,_:=regexp.Match(pattern,[]byte (str));ok{
+		fmt.Println("find")
+		fmt.Println(ok)
+
+	}else{
+		fmt.Println(ok)
+		fmt.Println("not find")
+	}
+*/
+
+
+
+
+
+
+}
+
+
+
 
 
