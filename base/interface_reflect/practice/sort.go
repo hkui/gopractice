d114470cc38ea25db6ac2d2cedce612c7b59438d
huangkui <huangkui@lepu.cn>
2019-04-26 09:03:42 +0800

    sort int[]  string[]

--- base/interface_reflect/practice/sort.go
+++ base/interface_reflect/practice/sort.go
@@ -1,25 +1,60 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 type sorter interface{
-	len()int
-	swap(i ,j int)
-	less(i,j int)bool
+	Len()int
+	Swap(i ,j int)
+	Less(i,j int)bool
 
 }
 type IntArray []int
 func (p IntArray) Len() int           { return len(p) }
-func (p IntArray) Less(i, j int) bool { return p[i] < p[j] }
+func (p IntArray) Less(i, j int) bool { return p[i] > p[j] }
 func (p IntArray) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+
+
+type StringArray []string
+
+
+func (p StringArray) Len() int           { return len(p) }
+func (p StringArray) Less(i, j int) bool { return p[i] > p[j] }
+func (p StringArray) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+
+func sort(data sorter)  {
+	for i:=0;i<data.Len()-1;i++{
+		for j:=i+1;j<data.Len() ;j++  {
+			if data.Less(i,j){
+				data.Swap(i,j)
+			}
+
+		}
+	}
+}
 
 func main()  {
 	data:=[]int{2,1,5,3,6,9,0}
-	a := sort.IntArray(data) //conversion to type IntArray from package sort
-	sort.Sort(a)
+	a := IntArray(data)
+	fmt.Println(a)
+	sort(a)
+	fmt.Println(a);
+
+
+	datastr:=[]string{"php","ajax","java"}
+	strarr:=StringArray(datastr)
+	fmt.Println(strarr[0])
+
+	fmt.Println(strarr)
+	sort(strarr)
+	fmt.Println(strarr)
+
+
+
+
+
+
 
 
 
