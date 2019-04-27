398fc2f7ccfe5710f9927539c109046694b2fd32
hk <764432054@qq.com>
2019-04-27 22:39:38 +0800

    deadline

--- base/routime_channel/deadlock.go
+++ base/routime_channel/deadlock.go
@@ -11,8 +11,10 @@ func f1(ch chan int)  {
 
 func main()  {
 	out:=make(chan int)
-	out<-2
+	out<-2  //挪到go下面就是对的了
 	go f1(out)
+
+
 	time.Sleep(1e9)
 }
 //fatal error: all goroutines are asleep - deadlock!
