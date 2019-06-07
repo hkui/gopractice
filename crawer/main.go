510de222dafdd72cb7dae20c37fa00d50ea3a423
hk <764432054@qq.com>
2019-06-07 22:37:30 +0800

    city

--- crawer/main.go
+++ crawer/main.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -29,7 +30,7 @@ func main() {
 	if err!=nil{
 		panic(err)
 	}
-	fmt.Printf("%s\n",bytes)
+	printCityList(bytes)
 
 }
 
@@ -41,4 +42,16 @@ func determineEncoding(r io.Reader) encoding.Encoding{
 	e,_,_ := charset.DetermineEncoding(bytes, "") //第二个返回值即为编码比如utf-8
 
 	return e
+}
+func printCityList(contents []byte)  {
+	//<a href="http://www.zhenai.com/zhenghun/aba" data-v-5e16505f="">阿坝</a>
+	res:= regexp.MustCompile(`<a \s*href="(http://www.zhenai.com/zhenghun/\w+)"[^>]*>([^<]+)</a>`)
+	matches := res.FindAllSubmatch(contents, -1) //[][]string
+	for _,m:=range matches{
+		fmt.Printf("url=>%s        name=>%s\n",m[1],m[2])
+
+		fmt.Println()
+
+	}
+
 }
