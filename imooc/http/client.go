9b97d866efbf1b99b82d2c7be53b06345bfd25df
hk <764432054@qq.com>
2019-06-07 14:32:57 +0800

    http

--- imooc/http/client.go
+++ imooc/http/client.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+)
+
 func main() {
-	$END$
+	request,err:= http.NewRequest(http.MethodGet,"http://www.imooc.com/",nil)
+	if err!=nil{
+		panic(err)
+	}
+	request.Header.Add("User-Agent", "Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Mobile Safari/537.36")
+	client:=http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("REdirect:",req)
+			return nil
+
+		},
+	}
+
+	resp,err:=client.Do(request)
+	if err!=nil{
+		panic(err)
+	}
+	defer resp.Body.Close()
+	s,err:=httputil.DumpResponse(resp,true)
+	if err!=nil{
+		panic(err)
+	}
+	fmt.Printf("s=%s\n",s)
 }
